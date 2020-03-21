a7a428701bb3adfe291d944a7413fbd1c375cdcb
AKTasos <tasos84@hotmail.com>
2020-03-21 15:28:19 +0100

    automatic fc layers
    nb of layers and  activation function as hyperparameters

--- tensorfromdata.py
+++ tensorfromdata.py
@@ -26,7 +26,7 @@ class TensorDataset(Dataset):
             self.y = torch.from_numpy(data.label.values)
         except :
             self.y = None
-        self.x = torch.from_numpy(data.loc[:,data.columns != "label"].values).unsqueeze(1).view(data.shape[0], 1, 28, 28)
+        self.x = torch.from_numpy(data.loc[:,data.columns != "label"].values).unsqueeze(1)
         self.n_samples=data.shape[0]
         
     def __getitem__(self, index):
--- test.py
+++ test.py
@@ -5,15 +5,72 @@ Created on Thu Mar 12 15:05:43 2020
 
 @author: aktasos
 """
+import torch.nn as nn
+import torch.nn.functional as F
 
-class test1():
+n_in = 784
+output = 10
+nb_of_layers = 10
+act_fonction="Relu"
+
+class FcLayers():
     
-    def __init__(self):
-        self.a=10
+    def __init__(self, n_in, output, nb_of_layers, act_fonction):
         
-class test2(test1):
-    def __init__(self):
-        self.b = test1.a
+        self.n_layer = 1
+        self.name = (f'fc{self.n_layer}')
+        self.in_features = n_in
+        self.n_delta = n_in // nb_of_layers
+        self.out_features = n_in-self.n_delta
+        self.output = output
+        self.nb_of_layers = nb_of_layers
+        self.layer_list = []
+        self.act_fonction="Relu"
+        
+    def next_layer_parameters(self):
+        
+        self.n_layer += 1
+        self.name = (f'fc{self.n_layer}')
+        self.in_features = self.out_features
+        self.out_features -= self.n_delta
+        
+        
+    def layer_creation(self):
+        for i in range(self.nb_of_layers):
+            if i == self.nb_of_layers-1 :
+                self.name = "out"
+                self.out_features = self.output
+            self.layer_list.append((self.name, self.in_features, self.out_features))
+            self.next_layer_parameters()
+            
+            
+    def network_creator(self):
+        
+        text_layer = str()
+        text_forward =str()
+        l=1
+        
+        for name, in_feat, out_feat in a.layer_list :
+            
+            text_layer =text_layer + f'self.{name} = nn.Linear(in_features= {in_feat}, out_features={out_feat}\n'
+            text_forward = text_forward + f'#layer {l} \nx=self.{name}(x) \nx=F.{self.act_fonction(x)}\n\n'
+            # layer_list.append(f'self.{name} = nn.Linear(in_features= {in_feat}, out_features={out_feat}')
+            # text_layer = '\n'.join(layer_list)
+            l += 1
+        
+        
     
-    def incremant (a):
-        test1.run_duration += 1
+        file = open("network_fc_template", "r")
+        text = file.read()
+        text.replace('layer_line', text_layer)
+        file = open(f"Network_{self.nb_of_layers}layers.py", "w")
+        file.write(first)
+        file.write(first)
+        file.write(first)
+        
+    
+    
+    
+        
+ a=fc_layers(n_in, output, nb_of_layers)
+a.layer_creation()
